5e87d217d902d81903df616fc8d421bd93912aff
LuminolT <copper_sulfate@qq.com>
2021-06-25 14:22:11 +0800

    Update Origin Bubble Sort

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@
 
 int main(){
     BubbleSort bs;
-    bs.data.setDataSize(255);
+    bs.data.setDataSize(1024);
     bs.data.getGaussDistribution(double(1));
-    for(int i = 0; i < 255; i++){
+    for(int i = 0; i < 1024; i++){
         std::cout << std::get<double>(bs.data.data[i]) << " ";
     }
     std::cout << std::endl << std::endl;
     std::vector<std::variant<int,double>> temp = bs.ori();
-    for(int i = 0; i < 255; i++){
+    for(int i = 0; i < 1024; i++){
         std::cout << std::get<double>(temp[i]) << " ";
     }
 

e232093db105b5bf17c6e59eea7a8ffefbf7483c
LuminolT <copper_sulfate@qq.com>
2021-07-01 11:12:52 +0800

    Add score sturct

---
+++ include/Score.h
@@ -0,0 +1,32 @@
+//
+// Created by admin on 2021/6/24.
+//
+
+#ifndef SORT_ALGORITHM_ANALYSIS_2021_STUDENT_H
+#define SORT_ALGORITHM_ANALYSIS_2021_STUDENT_H
+
+struct Score {
+    std::string ID;
+    int Chinese, Math, English, Physics, Chemistry, Total;
+    bool operator<(const Score &x) const {
+        return Total < x.Total;
+    }
+    bool operator==(const Score &x) const {
+        return Total == x.Total;
+    }
+    bool operator>(const Score &x) const {
+        return Total > x.Total;
+    }
+    bool operator<=(const Score &x) const {
+        return Total <= x.Total;
+    }
+    bool operator>=(const Score &x) const {
+        return Total >= x.Total;
+    }
+    friend std::ostream &operator<<(std::ostream& os, const Score &s) {
+        os << s.ID << ' ' << s.Total << '\n';
+        return os;
+    }
+};
+
+#endif //SORT_ALGORITHM_ANALYSIS_2021_STUDENT_H

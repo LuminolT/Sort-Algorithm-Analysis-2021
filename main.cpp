bf33901e0e8b7c209253365758e09f642ba49310
LuminolT <copper_sulfate@qq.com>
2021-07-03 02:36:47 +0800

    Give test example

--- main.cpp
+++ main.cpp
@@ -1,43 +1,29 @@
 #include <iostream>
-#include "Dataset.h"
-#include "BubbleSort.h"
-#include "SelectSort.h"
-#include "InsertSort.h"
-#include "QuickSort.h"
+#include "test.h"
 
+int size[] = {1024, 4096};
 
 int main() {
-    Dataset data;//
-    data.setDataSize(8);
-    data.setData(double(1.0), GAUSS);
-//    BubbleSort bs;
-//    InsertSort is;
-//    SelectSort ss;
-    QuickSort qs;
-//    std::cout <<"Bubble Sort:\n";
-//    bs.test(data);
-//    std::cout <<"\n\n\n";
-//    std::cout <<"Insert Sort:\n";
-//    is.test(data);
-//    std::cout <<"\n\n\n";
-//    std::cout <<"Select Sort:\n";
-//    ss.test(data);
-//    std::cout <<"\n\n\n";
-//    std::cout <<"QuickSort:\n";
-//    qs.test(data);
-//    std::cout <<"\n\n\n";
-//    return 0;
-//    Dataset d;
-//    d.setDataSize(4096);
-//    d.setData(double(1.0), UNIFORM);
-//    BubbleSort bs;
-//    bs.test(d);
-//    qs.setData(data);
-//    qs.ori();
-//    qs.printLog();
-//
-    qs.setData(data);
-    qs.improved();
-    qs.printLog();
-    getchar();
+    Test test;
+    std::cout << "UNIFORM\n";
+    for(auto i : size) {
+        std::cout << size << '\n';
+        test.startTest(i, UNIFORM);
+    }
+    std::cout << "GAUSS\n";
+    for(auto i : size) {
+        std::cout << size << '\n';
+        test.startTest(i, GAUSS);
+    }
+    std::cout << "UPPER\n";
+    for(auto i : size) {
+        std::cout << size << '\n';
+        test.startTest(i, UPPER);
+    }
+    std::cout << "UPPER\n";
+    for(auto i : size) {
+        std::cout << size << '\n';
+        test.startTest(i, UPPER);
+    }
+    return 0;
 }
